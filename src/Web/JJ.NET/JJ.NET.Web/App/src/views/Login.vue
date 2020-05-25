0495eca12bb2e7c0c38e9629089c7038851303ca
Jon <jonathan.jackson@sgsco.com>
2020-05-26 00:30:37 +0100

    Added styling, changed start-up address to be my local LAN address so hot reload works

--- src/Web/JJ.NET/JJ.NET.Web/App/src/views/Login.vue
+++ src/Web/JJ.NET/JJ.NET.Web/App/src/views/Login.vue
@@ -1,6 +1,23 @@
 <template>
     <div class="login">
-        LOGIN! test
+        <section class="hero is-primary is-fullheight">
+            <div class="hero-body">
+                <div class="container">
+                    <div class="columns is-centered">
+                        <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+                            <form action="" class="box">
+                                <p class="title is-3 has-text-primary">JJ.NET</p>
+                                <div class="field">
+                                    <button class="button is-success">
+                                       Login
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
